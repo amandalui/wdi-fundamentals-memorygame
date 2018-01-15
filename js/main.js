0a2ecf6daf38aa440b5676aa5117257fde0294c4
amandalui <hello@amandalui.com>
2018-01-15 13:26:13 -0800

    Unit 8 Assignment
    – Check if user flipped matching cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
+//var cardOne = "Queen";
+//var cardTwo = "King";
+//var cardThree = "Queen";
+//var cardFour = "King";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
+
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if ( cardsInPlay.length === 2 ) {
+    if ( cardsInPlay[0] === cardsInPlay[1] ) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
