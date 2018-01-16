44175d205d99546be1df0016d26a1ee6e7db2a44
amandalui <hello@amandalui.com>
2018-01-15 22:09:15 -0800

    Delete old code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-//var cardOne = "Queen";
-//var cardTwo = "King";
-//var cardThree = "Queen";
-//var cardFour = "King";
-
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardTwo);
-
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
 
@@ -18,13 +10,6 @@ var checkForMatch = function() {
     }
     };
 };
-
-//var cardOne = cards[0];
-//cardsInPlay.push(cardOne);
-//console.log("User flipped " + cardsInPlay);
-//var cardTwo = cards[1];
-//cardsInPlay.push(cardTwo);
-//console.log("User flipped " + cardsInPlay[1]);
 
 var flipCard = function(cardId) {
     console.log("User flipped " + cards[cardId]);
